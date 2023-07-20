3b6c7c111dbe61284bf5de446e88d2337409e69c
kvelardi <kristenvelardi@gmail.com>
2023-07-20 16:29:47 -0400

    completed the exercises.

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
+//Same Keys and values
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+  //same keys and values ES2015**** here is my refactoring****
 function createInstructor(firstName, lastName){
     return {
-      firstName: firstName,
-      lastName: lastName
+        firstName,
+        lastName
     }
-  }
-
-  //same keys and values ES2015
+};
 
 
-  var favoriteNumber = 42;
+//Computed Property Names
+//   var favoriteNumber = 42;
 
-var instructor = {
-  firstName: "Colt"
-}
+// var instructor = {
+//   firstName: "Colt"
+// }
 
-instructor[favoriteNumber] = "That is my favorite!"
+// instructor[favoriteNumber] = "That is my favorite!"
 
-//computedPropertyNames ES2015
+//computedPropertyNames ES2015 **** here is my refactoring****
+// let favoriteNumber = 42;
+// const instructor = {
+//     firstName: "Colt",
+//     [favoriteNumber]: "That is my favorite!"
+// };
 
 
-var instructor = {
+//Object Methods
+
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+
+  //ObjectMethods ES2015  **** here is my refactoring****
+  const instructor = {
     firstName: "Colt",
-    sayHi: function(){
-      return "Hi!";
+    sayHi(){
+        return "Hi!";
     },
-    sayBye: function(){
-      return this.firstName + " says bye!";
+    sayBye(){
+        return this.firstName + "says bye!";
     }
   }
-
-  //ObjectMethods ES2015
 
   //write a function which generates an animal object
   //accept 3 arguments: 
@@ -40,3 +64,18 @@ var instructor = {
     //const d = createAnimal('dog', 'bark', 'woof!')
         //{species: 'dog', bark: ƒ}
     //d.bark()//"woof!"
+
+
+    //****this is my function  */
+    function createAnimal (species, verb, noise){
+        return {
+            species, 
+            [verb](){
+                return noise;
+            }
+        }
+    };
+
+    const d = createAnimal("dog", "bark", "Woooof!")
+    // {species: "dog", bark: ƒ}
+    d.bark()  //"Woooof!"
